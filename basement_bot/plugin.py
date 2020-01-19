1cab4e64f4c8f4578e41edcbf709706ec4787817
effprime <exdoubledot@gmail.com>
2020-01-18 21:52:02 -0500

    Separate plugin loading to more methods

--- basement_bot/plugin.py
+++ basement_bot/plugin.py
@@ -19,15 +19,21 @@ class PluginLoader():
     def load_plugins(self):
         """Adds functions as commands from the plugins directory.
         """
-
-        wildcard = f"{join(dirname(__file__))}/plugins/*.py"
-        files = glob.glob(wildcard)
-        module_names = [ 
-            f"plugins.{basename(f)[:-3]}" for f in files if isfile(f) and not f.endswith('__init__.py')
-        ]
-        for module in module_names:
+        for module in self._get_modules():
             imported = importlib.import_module(module)
             for name, func in imported.__dict__.items():
                 if not name.startswith("__") and name != "commands":
                     logging.info(f"Loading command `{name}` from module {module}")
                     self.bot.add_command(func)
+
+    @staticmethod
+    def _get_modules():
+        """Gets the list of plugin modules.
+        """
+        files = glob.glob(
+            f"{join(dirname(__file__))}/plugins/*.py"
+        )
+        module_names = [ 
+            f"plugins.{basename(f)[:-3]}" for f in files if isfile(f) and not f.endswith('__init__.py')
+        ]
+        return module_names
