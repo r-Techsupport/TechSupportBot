e548d29cfea4e95b520608a42871880359896b3f
effprime <exdoubledot@gmail.com>
2020-01-19 21:55:03 -0500

    Revise factoid plugin

--- basement_bot/database.py
+++ basement_bot/database.py
@@ -6,6 +6,9 @@ from sqlalchemy.orm import sessionmaker
 
 
 class DatabaseHandler:
+    """Wrapper for SQLAlchemy functions.
+    """
+
     def __init__(self):
 
         env_info = self._get_env()
@@ -16,10 +19,14 @@ class DatabaseHandler:
         self.Base = declarative_base()
 
     def initialize(self):
+        """Wraps table creation.
+        """
         self.Base.metadata.create_all(self.engine)
 
     @staticmethod
     def _get_env():
+        """Gathers database environmental information.
+        """
         data = {}
         password = os.environ.get("POSTGRES_PASSWORD")
         if not password:
--- basement_bot/plugins/factoids.py
+++ basement_bot/plugins/factoids.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 from database import DatabaseHandler
 from sqlalchemy import Column, Integer, String
 
+from plugin import tagged_response
+
 db_handler = DatabaseHandler()
 
 
@@ -19,17 +21,62 @@ db_handler.initialize()
 def setup(bot):
     bot.add_command(add_factoid)
     bot.add_command(get_factoid)
+    bot.add_command(delete_factoid)
 
 
 @commands.command(name="r")
-async def add_factoid(ctx, arg1, arg2):
+async def add_factoid(ctx, arg1, *args):
+    if not args:
+        await tagged_response(ctx, "Factoids must not be blank!")
+        return
+
     db = db_handler.Session()
-    db.add(Factoid(text=arg1.lower(), message=arg2))
-    db.commit()
 
+    try:
+        # first check if key already exists
+        entry = db.query(Factoid).filter(Factoid.text == arg1).first()
+        if entry:
+            # delete old one
+            db.delete(entry)
+            await tagged_response(ctx, "Deleting previous entry of factoid trigger...")
 
-@commands.command(name="getf")
+        # finally, add new entry
+        db.add(Factoid(text=arg1.lower(), message=" ".join(args)))
+        db.commit()
+        await tagged_response(ctx, f"Successfully added factoid trigger: *{arg1}*")
+
+    except Exception:
+        await tagged_response(ctx, "I ran into an issue handling your factoid addition...")
+        raise RuntimeError("Error handling new factoid information")
+        
+
+@commands.command(name="f")
+async def delete_factoid(ctx, arg):
+    db = db_handler.Session()
+
+    try:
+        entry = db.query(Factoid).filter(Factoid.text == arg).first()
+        if entry:
+            db.delete(entry)
+            db.commit()
+        await tagged_response(ctx, f"Successfully deleted factoid trigger: *{arg}*")
+
+    except Exception:
+        await tagged_response(ctx, "I ran into an issue handling your factoid deletion...")
+        raise RuntimeError("Error querying/deleting factoid")
+        
+
+@commands.command(name="q")
 async def get_factoid(ctx, arg):
     db = db_handler.Session()
-    for f in db.query(Factoid).filter(Factoid.text == arg):
-        await ctx.send(f.message)
+
+    try:
+        entry = db.query(Factoid).filter(Factoid.text == arg).first()
+        if entry:
+            await tagged_response(ctx, entry.message)
+        else:
+            await tagged_response(ctx, "No factoid exists!")
+
+    except Exception:
+        await tagged_response(ctx, "I ran into an issue grabbing your factoid...")
+        raise RuntimeError("Error grabbing factoid")
