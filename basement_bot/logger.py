a4f63c018eee271c51a4bd9d97de887d0552913b
effprime <exdoubledot@gmail.com>
2020-01-23 22:26:19 -0500

    Add documentation to logger file

--- basement_bot/logger.py
+++ basement_bot/logger.py
@@ -1,9 +1,17 @@
+"""Module for generating a standard logger.
+"""
+
 import logging
 
 from utils import get_env_value
 
 
 def get_logger(name):
+    """Gathers a logger object based on project standards.
+
+    parameters:
+        name (str): the name for the logger.
+    """
     debug = get_env_value("DEBUG", raise_exception=False)
     level = logging.DEBUG if debug else logging.INFO
     formatting = "%(asctime)s [%(name)s, %(levelname)s]: %(message)s"
