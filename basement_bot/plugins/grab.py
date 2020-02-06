ccfb2f78383ad652893f1bb8d6d439c1400ae10c
effprime <exdoubledot@gmail.com>
2020-02-05 20:00:19 -0500

    Add channel fields to factoids schema

--- basement_bot/plugins/grab.py
+++ basement_bot/plugins/grab.py
@@ -4,17 +4,17 @@ from random import randint
 from discord.ext import commands
 from sqlalchemy import Column, DateTime, Integer, String
 
-from utils.database import DatabaseHandler
+from utils.database import DatabaseHandle
 from utils.helpers import get_env_value, tagged_response
 
-db_handler = DatabaseHandler()
+db_handle = DatabaseHandle()
 
 SEARCH_LIMIT = 50
 
 COMMAND_PREFIX = get_env_value("COMMAND_PREFIX")
 
 
-class Grab(db_handler.Base):
+class Grab(db_handle.Base):
     __tablename__ = "grabs"
 
     pk = Column(Integer, primary_key=True)
@@ -24,7 +24,7 @@ class Grab(db_handler.Base):
     time = Column(DateTime, default=datetime.datetime.utcnow)
 
 
-db_handler.create_all()
+db_handle.create_all()
 
 
 def setup(bot):
@@ -35,7 +35,7 @@ def setup(bot):
 
 @commands.command(name="grab")
 async def grab(ctx):
-    channel = ctx.message.channel
+    channel = str(ctx.message.channel.id)
     user_to_grab = ctx.message.mentions[0] if ctx.message.mentions else None
 
     if not user_to_grab:
@@ -59,14 +59,14 @@ async def grab(ctx):
             ctx, f"Could not find a recent essage from user {user_to_grab}"
         )
 
-    db = db_handler.Session()
+    db = db_handle.Session()
 
     try:
         if (
             db.query(Grab)
             .filter(
                 Grab.author_id == str(user_to_grab.id),
-                Grab.channel == str(channel.id),
+                Grab.channel == channel,
                 Grab.message == grab_message,
             )
             .count()
@@ -75,21 +75,17 @@ async def grab(ctx):
             await tagged_response(ctx, "That grab already exists!")
             return
         db.add(
-            Grab(
-                author_id=str(user_to_grab.id),
-                channel=str(channel.id),
-                message=grab_message,
-            )
+            Grab(author_id=str(user_to_grab.id), channel=channel, message=grab_message,)
         )
         db.commit()
-        await tagged_response(ctx, f"'*Successfully saved: {grab_message}*'")
-    except Exception as e:
+        await tagged_response(ctx, f"Successfully saved: '*{grab_message}*'")
+    except Exception:
         await tagged_response(ctx, "I had an issue remembering that message!")
 
 
 @commands.command(name="grabs")
 async def get_grabs(ctx):
-    channel = ctx.message.channel
+    channel = str(ctx.message.channel.id)
     user_to_grab = ctx.message.mentions[0] if ctx.message.mentions else None
 
     if not user_to_grab:
@@ -100,47 +96,50 @@ async def get_grabs(ctx):
         await tagged_response(ctx, "Ain't gonna catch me slipping!")
         return
 
-    db = db_handler.Session()
+    db = db_handle.Session()
 
     try:
         grabs = db.query(Grab).filter(
-            Grab.author_id == str(user_to_grab.id), Grab.channel == str(channel.id)
+            Grab.author_id == str(user_to_grab.id), Grab.channel == channel
         )
         if grabs:
             message = ""
             for grab_ in grabs[:-1]:
                 message += f"'*{grab_.message}*', "
-            message += f"and '*{grabs[-1].message}*'"
+            if message:
+                message += f"and '*{grabs[-1].message}*'"
+            else:
+                message = f"'*{grabs[-1].message}*'"
         else:
             message = f"No messages found for {user_to_grab}"
         await tagged_response(ctx, message)
-    except Exception as e:
+    except Exception:
         return
 
 
 @commands.command(name="grabr")
 async def random_grab(ctx):
-    channel = ctx.message.channel
+    channel = str(ctx.message.channel.id)
     user_to_grab = ctx.message.mentions[0] if ctx.message.mentions else None
 
     if user_to_grab and user_to_grab.bot:
         await tagged_response(ctx, "Ain't gonna catch me slipping!")
         return
 
-    db = db_handler.Session()
+    db = db_handle.Session()
 
     try:
         if user_to_grab:
             grabs = db.query(Grab).filter(
-                Grab.author_id == str(user_to_grab.id), Grab.channel == str(channel.id)
+                Grab.author_id == str(user_to_grab.id), Grab.channel == channel
             )
         else:
-            grabs = db.query(Grab).filter(Grab.channel == str(channel.id))
+            grabs = db.query(Grab).filter(Grab.channel == channel)
         if grabs:
             random_index = randint(0, grabs.count() - 1)
             message = f"'*{grabs[random_index].message}*'"
         else:
             message = f"No messages found for {user_to_grab}"
         await tagged_response(ctx, message)
-    except Exception as e:
+    except Exception:
         return
