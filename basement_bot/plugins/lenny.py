3b5eab472cac2fa2d653a3542ca88c4fedfc6921
effprime <exdoubledot@gmail.com>
2020-06-27 14:45:04 -0400

    migrate lenny plugin to cog

--- basement_bot/plugins/lenny.py
+++ basement_bot/plugins/lenny.py
@@ -2,30 +2,42 @@ from random import choice
 
 from discord.ext import commands
 
+from utils.cogs import BasicPlugin
+
 
 def setup(bot):
-    bot.add_command(lenny)
+    bot.add_cog(Lenny(bot))
 
 
-@commands.command(
-    name="len",
-    brief="( ͡° ͜ʖ ͡°)",
-    description="Returns a randomly chosen Lenny face.",
-    usage="",
-    help="\nLimitations: Ignores any plain text or mentions after the command.",
-)
-async def lenny(ctx):
-    await ctx.send(choice(lennys))
+class Lenny(BasicPlugin):
 
+    LENNYS_SELECTION = [
+        "( ͡° ͜ʖ ͡°)",
+        "( ͠° ͟ʖ ͡°)",
+        "( ͡ʘ ͜ʖ ͡ʘ)",
+        "(° ͜ʖ °)",
+        "ಠ_ಠ",
+        "( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)",
+        "༼  ͡° ͜ʖ ͡° ༽",
+        "(͡ ͡° ͜ つ ͡͡°)",
+        "[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅]",
+        "( ͡o ͜ʖ ͡o)",
+        "(⟃ ͜ʖ ⟄)",
+        "( ͜。 ͡ʖ ͜。)",
+        "( ͡°⊖ ͡°)",
+        "✧･ﾟ: *✧･ﾟ♡*( ͡˘̴ ͜ ʖ̫ ͡˘̴ )*♡･ﾟ✧*:･ﾟ✧",
+        "°。°。°。°( ͡° ͜ʖ ͡ °)°。°。°。°",
+        "┐( ͡ಠ ʖ̯ ͡ಠ)┌",
+        "ʕ ͡° ʖ̯ ͡°ʔ",
+        "╭∩╮( ͡° ل͟ ͡° )╭∩╮",
+    ]
 
-lennys = [
-    "( ͡° ͜ʖ ͡°)",
-    "( ͠° ͟ʖ ͡°)",
-    "( ͡ʘ ͜ʖ ͡ʘ)",
-    "(° ͜ʖ °)",
-    "ಠ_ಠ",
-    "( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)",
-    "༼  ͡° ͜ʖ ͡° ༽",
-    "(͡ ͡° ͜ つ ͡͡°)",
-    "[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅]",
-]
+    @commands.command(
+        name="len",
+        brief="( ͡° ͜ʖ ͡°)",
+        description="Returns a randomly chosen Lenny face.",
+        usage="",
+        help="\nLimitations: Ignores any plain text or mentions after the command.",
+    )
+    async def lenny(self, ctx):
+        await ctx.send(choice(self.LENNYS_SELECTION))
