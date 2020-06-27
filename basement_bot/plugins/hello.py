ce7031d6be187a232c2cb64b8eff7e485ccfab81
effprime <exdoubledot@gmail.com>
2020-06-27 14:35:15 -0400

    migrate hello plugin to cog

--- basement_bot/plugins/hello.py
+++ basement_bot/plugins/hello.py
@@ -1,19 +1,21 @@
 from discord.ext import commands
 
+from utils.cogs import BasicPlugin
 from utils.helpers import emoji_reaction
 
 
 def setup(bot):
-    bot.add_command(hello)
+    bot.add_cog(Greeter(bot))
 
 
-@commands.command(
-    name="hello",
-    brief="Hello!",
-    description="Returns the greeting 'HEY' as a reaction to the original command message.",
-    usage="",
-)
-async def hello(ctx):
-    # H, E, Y
-    emojis = [u"\U0001F1ED", u"\U0001F1EA", u"\U0001F1FE"]
-    await emoji_reaction(ctx, emojis)
+class Greeter(BasicPlugin):
+    @commands.command(
+        name="hello",
+        brief="Hello!",
+        description="Returns the greeting 'HEY' as a reaction to the original command message.",
+        usage="",
+    )
+    async def hello(self, ctx):
+        # H, E, Y
+        emojis = [u"\U0001F1ED", u"\U0001F1EA", u"\U0001F1FE"]
+        await emoji_reaction(ctx, emojis)
