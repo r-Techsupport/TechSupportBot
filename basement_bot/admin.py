e92a0a060dc5b81fb2aaa7021fc6ecfc71a3ecbe
effprime <exdoubledot@gmail.com>
2020-12-28 18:22:21 -0500

    clean up admin cog

--- basement_bot/admin.py
+++ basement_bot/admin.py
@@ -3,7 +3,8 @@
 
 from cogs import BasicPlugin
 from discord.ext import commands
-from utils.helpers import embed_from_kwargs, priv_response
+from utils.embed import SafeEmbed
+from utils.helpers import paginate, tagged_response
 
 
 class AdminControl(BasicPlugin):
@@ -30,30 +31,30 @@ class AdminControl(BasicPlugin):
 
         error = status_data.get("error")
         if error:
-            await priv_response(ctx, f"Error: {error}")
+            await tagged_response(ctx, f"Error: {error}")
             return
 
         if plugin_name:
-            if plugin_name in status_data.get("loaded", []):
-                await priv_response(ctx, f"Plugin `{plugin_name}` is loaded")
-            else:
-                await priv_response(ctx, f"Plugin `{plugin_name}` is not loaded")
+            status = status_data.get(plugin_name)
+            if not status:
+                await tagged_response(ctx, "Plugin not found!")
+                return
+
+            await tagged_response(ctx, f"Plugin is {status}")
             return
 
-        loaded = [f"`{plugin}`" for plugin in status_data.get("loaded")]
-        loaded = ", ".join(loaded) if loaded else "*None*"
-        unloaded = [f"`{plugin}`" for plugin in status_data.get("unloaded")]
-        unloaded = ", ".join(unloaded) if unloaded else "*None*"
-        disabled = [f"`{plugin}`" for plugin in status_data.get("disabled")]
-        disabled = ", ".join(disabled) if disabled else "*None*"
+        embeds = []
+        field_counter = 1
+        for index, key in enumerate(list(status_data.keys())):
+            embed = SafeEmbed(title="Plugin Status") if field_counter == 1 else embed
+            embed.add_field(name=key, value=status_data[key], inline=False)
+            if field_counter == 5 or index == len(status_data) - 1:
+                embeds.append(embed)
+                field_counter = 1
+            else:
+                field_counter += 1
 
-        await priv_response(
-            ctx,
-            embed=embed_from_kwargs(
-                title="Plugin Status",
-                **{"Loaded": loaded, "Unloaded": unloaded, "Disabled": disabled},
-            ),
-        )
+        await paginate(ctx, embeds)
 
     @commands.command(hidden=True)
     async def load_plugin(self, ctx, *args):
@@ -68,15 +69,11 @@ class AdminControl(BasicPlugin):
         plugin_name = args[0].lower() if args else None
 
         if not plugin_name:
-            await priv_response(ctx, "Invalid input")
-            return
-
-        if not plugin_name.isalpha():
-            await priv_response(ctx, "Plugin name must be letters only")
+            await tagged_response(ctx, "Invalid input")
             return
 
         response = ctx.bot.plugin_api.load_plugin(plugin_name)
-        await priv_response(ctx, response.message)
+        await tagged_response(ctx, response.message)
 
     @commands.command(hidden=True)
     async def unload_plugin(self, ctx, *args):
@@ -91,15 +88,11 @@ class AdminControl(BasicPlugin):
         plugin_name = args[0].lower() if args else None
 
         if not plugin_name:
-            await priv_response(ctx, "Invalid input")
-            return
-
-        if not plugin_name.isalpha():
-            await priv_response(ctx, "Plugin name must be letters only")
+            await tagged_response(ctx, "Invalid input")
             return
 
         response = ctx.bot.plugin_api.unload_plugin(plugin_name)
-        await priv_response(ctx, response.message)
+        await tagged_response(ctx, response.message)
 
     @commands.command(hidden=True)
     async def enable_command(self, ctx, *args):
@@ -113,22 +106,20 @@ class AdminControl(BasicPlugin):
         """
         command_name = args[0].lower() if args else None
         if not command_name:
-            await priv_response(ctx, "Invalid input")
+            await tagged_response(ctx, "Invalid input")
             return
 
         command_ = ctx.bot.get_command(command_name)
         if not command_:
-            await priv_response(ctx, f"No such command: `{command_name}`")
-        else:
-            if not command_.enabled:
-                command_.enabled = True
-                await priv_response(
-                    ctx, f"Successfully enabled command: `{command_name}`"
-                )
-            else:
-                await priv_response(
-                    ctx, f"Command `{command_name}` is already enabled!"
-                )
+            await tagged_response(ctx, f"No such command: `{command_name}`")
+            return
+
+        if command_.enabled:
+            await tagged_response(ctx, f"Command `{command_name}` is already enabled!")
+            return
+
+        command_.enabled = True
+        await tagged_response(ctx, f"Successfully enabled command: `{command_name}`")
 
     @commands.command(hidden=True)
     async def disable_command(self, ctx, *args):
@@ -142,22 +133,20 @@ class AdminControl(BasicPlugin):
         """
         command_name = args[0].lower() if args else None
         if not command_name:
-            await priv_response(ctx, "Invalid input")
+            await tagged_response(ctx, "Invalid input")
             return
 
         command_ = ctx.bot.get_command(command_name)
         if not command_:
-            await priv_response(ctx, f"No such command: `{command_name}`")
-        else:
-            if not command_.disabled:
-                command_.disabled = True
-                await priv_response(
-                    ctx, f"Successfully disabled command: `{command_name}`"
-                )
-            else:
-                await priv_response(
-                    ctx, f"Command `{command_name}` is already disabled!"
-                )
+            await tagged_response(ctx, f"No such command: `{command_name}`")
+            return
+
+        if not command_.enabled:
+            await tagged_response(ctx, f"Command `{command_name}` is already disabled!")
+            return
+
+        command_.enabled = False
+        await tagged_response(ctx, f"Successfully disabled command: `{command_name}`")
 
     @commands.command(hidden=True)
     async def set_game(self, ctx, *args):
@@ -173,10 +162,10 @@ class AdminControl(BasicPlugin):
 
         if not all(char == " " for char in game_):
             await ctx.bot.set_game(game_)
-            await priv_response(ctx, f"Successfully set game to: *{game_}*")
+            await tagged_response(ctx, f"Successfully set game to: *{game_}*")
 
         else:
-            await priv_response(ctx, "I cannot play a game with no name!")
+            await tagged_response(ctx, "I cannot play a game with no name!")
 
     @commands.command(name="delete_bot_x", hidden=True)
     async def delete_x_bot_messages(self, ctx, *args):
@@ -189,7 +178,7 @@ class AdminControl(BasicPlugin):
             args [list]: the space-or-quote-delimitted args
         """
         if not args:
-            await priv_response(ctx, "Please provide a number of messages to delete")
+            await tagged_response(ctx, "Please provide a number of messages to delete")
             return
 
         try:
@@ -199,8 +188,6 @@ class AdminControl(BasicPlugin):
         except Exception:
             amount = 1
 
-        await priv_response(ctx, "Starting bot history deletion...")
-
         counter = 0
         async for message in ctx.channel.history(limit=500):
             if counter >= amount:
@@ -209,7 +196,7 @@ class AdminControl(BasicPlugin):
                 await message.delete()
                 counter += 1
 
-        await priv_response(
+        await tagged_response(
             ctx,
             f"I finished trying to delete {amount} of my most recent messages in that channel",
         )
@@ -225,7 +212,7 @@ class AdminControl(BasicPlugin):
             args [list]: the space-or-quote-delimitted args
         """
         if not args:
-            await priv_response(ctx, "Please provide a number of messages to delete")
+            await tagged_response(ctx, "Please provide a number of messages to delete")
             return
 
         try:
@@ -235,8 +222,6 @@ class AdminControl(BasicPlugin):
         except Exception:
             amount = 1
 
-        await priv_response(ctx, "Starting total deletion...")
-
         counter = 0
         async for message in ctx.channel.history(limit=500):
             if counter >= amount:
@@ -244,7 +229,7 @@ class AdminControl(BasicPlugin):
             await message.delete()
             counter += 1
 
-        await priv_response(
+        await tagged_response(
             ctx,
             f"I finished trying to delete {amount} most recent messages in that channel",
         )
@@ -262,12 +247,12 @@ class AdminControl(BasicPlugin):
         """
         channel = self.bot.get_channel(int(channel_id))
         if not channel:
-            await priv_response(ctx, "I couldn't find that channel")
+            await tagged_response(ctx, "I couldn't find that channel")
             return
 
         message = " ".join(args)
         if not message:
-            await priv_response(ctx, "I need a message to echo")
+            await tagged_response(ctx, "I need a message to echo")
             return
 
         await channel.send(content=message)
@@ -285,12 +270,12 @@ class AdminControl(BasicPlugin):
         """
         user = await self.bot.fetch_user(int(user_id))
         if not user:
-            await priv_response(ctx, "I couldn't find that user")
+            await tagged_response(ctx, "I couldn't find that user")
             return
 
         message = " ".join(args)
         if not message:
-            await priv_response(ctx, "I need a message to echo")
+            await tagged_response(ctx, "I need a message to echo")
             return
 
         await user.send(content=message)
