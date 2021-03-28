3ccf54d891b7df8f89636f705e4ca6cf2399b426
Jalad25 <>
2021-03-27 20:22:56 -0500

    Added suggestions

--- basement_bot/admin.py
+++ basement_bot/admin.py
@@ -15,6 +15,8 @@ class AdminControl(base.BaseCog):
 
     ADMIN_ONLY = True
 
+    GITHUB_API_BASE_URL = "https://api.github.com"
+
     @commands.group(
         name="plugin",
         brief="Executes a plugin bot command",
@@ -318,16 +320,14 @@ class AdminControl(base.BaseCog):
             title: the title of the issue
             description: the description of the issue
         """
-        GITHUB_API_BASE_URL = "https://api.github.com"
+
         ICON_URL = (
             "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
         )
 
         oauth_token = self.bot.config.main.api_keys.github
         if not oauth_token:
-            await self.bot.tagged_response(
-                ctx, "I couldn't authenticate with Github", target=ctx.author
-            )
+            await self.bot.tagged_response(ctx, "I couldn't authenticate with Github")
             return
 
         headers = {
@@ -336,37 +336,33 @@ class AdminControl(base.BaseCog):
             "Content-Type": "text/plain",
         }
 
-        data = {"title": f"{title}", "body": f"{description}"}
+        data = {"title": title, "body": description}
 
         username = self.bot.config.special.github.username
         repo = self.bot.config.special.github.repo
         if not username or not repo:
-            await self.bot.tagged_response(
-                ctx, "I couldn't find the repository", target=ctx.author
-            )
+            await self.bot.tagged_response(ctx, "I couldn't find the repository")
             return
-        route = f"/repos/{username}/{repo}/issues"
+        route = f"{self.GITHUB_API_BASE_URL}/repos/{username}/{repo}/issues"
 
         response = await self.bot.http_call(
             "post",
-            GITHUB_API_BASE_URL + route,
+            route,
             headers=headers,
             data=json.dumps(data),
         )
 
         if response.get("status_code") != 201:
             await self.bot.tagged_response(
-                ctx,
-                "I was unable to create your issue. Please try again later.",
-                target=ctx.author,
+                ctx, "I was unable to create your issue. Please try again later."
             )
             return
 
-        issueUrl = response.get("html_url")
+        issue_url = response.get("html_url")
         number = response.get("number")
 
         embed = self.bot.embed_api.Embed(title="Issue Created")
-        embed.add_field(name=f"Issue #{number}", value=f"{issueUrl}")
+        embed.add_field(name=f"Issue #{number}", value=f"{issue_url}")
         embed.set_thumbnail(url=ICON_URL)
 
-        await self.bot.tagged_response(ctx, embed=embed, target=ctx.author)
+        await self.bot.tagged_response(ctx, embed=embed)
