b50ae48684b0ae2e91e570cdc6b946e86e342a23
TheKrol <TheKrol@users.noreply.github.com>
2023-02-05 17:58:44 -0500

    Update rules.py

--- techsupport_bot/extensions/rules.py
+++ techsupport_bot/extensions/rules.py
@@ -1,3 +1,4 @@
+"""Module for the rules extension of the discord bot."""
 import datetime
 import io
 import json
@@ -9,25 +10,31 @@ from discord.ext import commands
 
 
 def setup(bot):
+    """Adding the rules configuration to the config file."""
     bot.add_cog(Rules(bot=bot))
 
 
 class RuleEmbed(discord.Embed):
+    """Class for setting up the rules embed."""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.color = discord.Color.gold()
 
 
 class Rules(base.BaseCog):
+    """Class to define the rules for the extension."""
+
     RULE_ICON_URL = "https://cdn.icon-icons.com/icons2/907/PNG/512/balance-scale-of-justice_icon-icons.com_70554.png"
     COLLECTION_NAME = "rules_extension"
 
     async def preconfig(self):
+        """Method to preconfig the rules."""
         if not self.COLLECTION_NAME in await self.bot.mongo.list_collection_names():
             await self.bot.mongo.create_collection(self.COLLECTION_NAME)
 
     @commands.group(name="rule")
     async def rule_group(self, ctx):
+        """Method for the rule group."""
         pass
 
     @commands.has_permissions(administrator=True)
@@ -39,6 +46,7 @@ class Rules(base.BaseCog):
         usage="|uploaded-json|",
     )
     async def edit_rules(self, ctx):
+        """Method to edit the rules that were set up."""
         collection = self.bot.mongo[self.COLLECTION_NAME]
 
         uploaded_data = await util.get_json_from_attachments(ctx.message)
@@ -74,53 +82,38 @@ class Rules(base.BaseCog):
         description="Gets a rule by number for the current server",
         usage="[number]",
     )
-    async def get_rule(self, ctx, content: str):
-        """Method to get specified rules from rule number/s specified in content."""
-        first = True
+    async def get_rule(self, ctx, number: int):
+        """Method to get a specific rule on command."""
+        if number < 1:
+            await ctx.send_deny_embed("That rule number is invalid")
+            return
 
-        numbers = []
+        rules_data = await self.bot.mongo[self.COLLECTION_NAME].find_one(
+            {"guild_id": {"$eq": str(ctx.guild.id)}}
+        )
+        if not rules_data or not rules_data.get("rules"):
+            await ctx.send_deny_embed("There are no rules for this server")
+            return
 
-        # Splits content string, and adds each item to number list
-        numbers.extend([int(num) for num in content.split(",")])
+        rules = rules_data.get("rules")
 
-        for number in numbers:
-            if number < 1:
-                await ctx.send_deny_embed("That rule number is invalid")
-                return
+        try:
+            rule = rules[number - 1]
+        except IndexError:
+            rule = None
 
-            rules_data = await self.bot.mongo[self.COLLECTION_NAME].find_one(
-                {"guild_id": {"$eq": str(ctx.guild.id)}}
-            )
+        if not rule:
+            await ctx.send_deny_embed("That rule number doesn't exist")
+            return
 
-            if not rules_data or not rules_data.get("rules"):
-                await ctx.send_deny_embed("There are no rules for this server")
-                return
-            rules = rules_data.get("rules")
+        embed = RuleEmbed(
+            title=f"Rule {number}", description=rule.get("description", "None")
+        )
 
-            try:
-                rule = rules[number - 1]
-            except IndexError:
-                rule = None
+        embed.set_thumbnail(url=self.RULE_ICON_URL)
+        embed.color = discord.Color.gold()
 
-            if not rule:
-                await ctx.send_deny_embed(f"Rule number {number} doesn't exist")
-                return
-
-            embed = RuleEmbed(
-                title=f"Rule {number}", description=rule.get("description", "None")
-            )
-
-            embed.set_thumbnail(url=self.RULE_ICON_URL)
-            embed.color = discord.Color.gold()
-
-            # Checks if first embed sent
-            if first:
-                await ctx.send(
-                    embed=embed, targets=ctx.message.mentions or [ctx.author]
-                )
-                first = False
-            else:
-                await ctx.send(embed=embed, mention_author=False)
+        await ctx.send(embed=embed)
 
     @commands.guild_only()
     @rule_group.command(
@@ -129,6 +122,7 @@ class Rules(base.BaseCog):
         description="Gets all the rules for the current server",
     )
     async def get_all_rules(self, ctx):
+        """Method to get all the rules that are set up."""
         rules_data = await self.bot.mongo[self.COLLECTION_NAME].find_one(
             {"guild_id": {"$eq": str(ctx.guild.id)}}
         )
@@ -151,4 +145,4 @@ class Rules(base.BaseCog):
         embed.set_thumbnail(url=self.RULE_ICON_URL)
         embed.color = discord.Color.gold()
 
-        await ctx.send(embed=embed, mention_author=False)
+        await ctx.send(embed=embed)
