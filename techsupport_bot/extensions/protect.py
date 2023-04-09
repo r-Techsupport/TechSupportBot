eea55709c01cc63273f639f6ad1a1d3d7d0d3d5d
Cpt-Dingus <meti17207@gmail.com>
2023-04-09 23:14:52 +0200

    It's possible to ban users not in the server again

--- techsupport_bot/extensions/protect.py
+++ techsupport_bot/extensions/protect.py
@@ -528,9 +528,15 @@ class Protector(base.MatchCog):
         description="Bans a user with a given reason",
         usage="@user [reason]",
     )
-    async def ban_user(self, ctx, user: discord.Member, *, reason: str = None):
+    async def ban_user(self, ctx, user: discord.User, *, reason: str = None):
         """Method to ban a user from discord."""
-        await self.handle_ban(ctx, user, reason)
+
+        # Uses the discord.Member class to get the top role attribute if the
+        # user is a part of the target guild
+        if ctx.guild.get_member(user.id) is not None:
+            await self.handle_ban(ctx, ctx.guild.get_member(user.id), reason)
+        else:
+            await self.handle_ban(ctx, user, reason)
 
         config = await self.bot.get_context_config(ctx)
         await self.send_alert(config, ctx, "Ban command")
@@ -543,9 +549,15 @@ class Protector(base.MatchCog):
         description="Unbans a user with a given reason",
         usage="@user [reason]",
     )
-    async def unban_user(self, ctx, user: discord.Member, *, reason: str = None):
+    async def unban_user(self, ctx, user: discord.User, *, reason: str = None):
         """Method to unban a user from discord."""
-        await self.handle_unban(ctx, user, reason)
+
+        # Uses the discord.Member class to get the top role attribute if the
+        # user is a part of the target guild
+        if ctx.guild.get_member(user.id) is not None:
+            await self.handle_unban(ctx, ctx.guild.get_member(user.id), reason)
+        else:
+            await self.handle_unban(ctx, user, reason)
 
     @commands.has_permissions(kick_members=True)
     @commands.bot_has_permissions(kick_members=True)
@@ -597,7 +609,7 @@ class Protector(base.MatchCog):
         description="Gets warnings for a user",
         usage="@user",
     )
-    async def get_warnings_command(self, ctx, user: discord.Member):
+    async def get_warnings_command(self, ctx, user: discord.User):
         """Method to get the warnings of a user in discord."""
         warnings = await self.get_warnings(user, ctx.guild)
         if not warnings:
