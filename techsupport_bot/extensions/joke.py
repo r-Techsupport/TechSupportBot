b6c38e6014099c8f808210b211a48895f939e8aa
TheKrol <TheKrol@users.noreply.github.com>
2023-05-13 06:09:23 -0400

    Update joke.py
    update avatar for 2.0

--- techsupport_bot/extensions/joke.py
+++ techsupport_bot/extensions/joke.py
@@ -46,7 +46,7 @@ class Joker(base.BaseCog):
     def generate_embed(self, joke_text):
         """Method to generate the embed to send to discord for displaying joke."""
         embed = discord.Embed(description=joke_text)
-        embed.set_author(name="Joke", icon_url=self.bot.user.avatar_url)
+        embed.set_author(name="Joke", icon_url=self.bot.user.avatar.url)
         embed.color = discord.Color.random()
         return embed
 
