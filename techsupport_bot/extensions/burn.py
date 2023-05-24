94e07912ce1cf72a48f5900f1085ec0498c643b9
Ajax146 <31014239+ajax146@users.noreply.github.com>
2023-05-24 22:57:41 +0000

    Refactor burn, make mock discord objects, make unit tests

--- techsupport_bot/extensions/burn.py
+++ techsupport_bot/extensions/burn.py
@@ -12,15 +12,6 @@ async def setup(bot):
     await bot.add_cog(Burn(bot=bot))
 
 
-class BurnEmbed(discord.Embed):
-    """Method to create the burn command discord embed."""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.title = "Burn Alert!"
-        self.color = discord.Color.red()
-
-
 class Burn(base.BaseCog):
     """Class for Burn command on the discord bot."""
 
@@ -34,6 +25,59 @@ class Burn(base.BaseCog):
         "Was that message a hot pan? BECAUSE IT BURNS!",
     ]
 
+    def generate_burn_embed(self):
+        """
+        This generates a burn embed properly styled
+
+        Returns:
+            discord.Embed: The styled embed, fully ready for sending
+        """
+        embed = discord.Embed(title="Burn Alert!")
+        embed.color = discord.Color.red()
+        message = random.choice(self.PHRASES)
+        embed.description = f"🔥🔥🔥 {message} 🔥🔥🔥"
+        return embed
+
+    async def handle_burn(self, ctx, user: discord.Member, message: discord.Message):
+        """The core logic to handle the burn command
+
+        Args:
+            ctx (commands.Context): The context in which the command was run in
+            user (discord.Member): The user that was called in the burn command
+            message (discord.Message): The message to react to. Will be None if no message could be found
+
+        Error handling:
+            No message found: send_deny_embed
+        """
+        if not message:
+            await ctx.send_deny_embed("I could not a find a message to reply to")
+            return
+
+        for emoji in ["🔥", "🚒", "👨‍🚒"]:
+            await message.add_reaction(emoji)
+
+        embed = self.generate_burn_embed()
+        await ctx.send(embed=embed, targets=[user])
+
+    async def get_message(self, ctx, prefix: str, user: discord.Member):
+        """Gets a message from the channel history to burn
+
+        Args:
+            ctx (commands.Context): The context in which the burn command was run in
+            prefix (str): The current bot prefix. This is used to ignore command messages
+            user (discord.Member): The member to search for a message from
+
+        Returns:
+            str: The matched message. Will be None if no message exists within the SEARCH_LIMT
+        """
+        matched_message = None
+
+        async for message in ctx.channel.history(limit=self.SEARCH_LIMIT):
+            if message.author == user and not message.content.startswith(prefix):
+                matched_message = message
+                break
+        return matched_message
+
     @util.with_typing
     @commands.guild_only()
     @commands.command(
@@ -42,25 +86,15 @@ class Burn(base.BaseCog):
         usage="@user",
     )
     async def burn(self, ctx, user_to_match: discord.Member):
-        """Method to apply the burn command on discord bot."""
-        matched_message = None
+        """The function executed when .burn is run on discord
+        This is a command and should be accessed via discord
+
+        Args:
+            ctx (commands.Context): The context in which the command was run
+            user_to_match (discord.Member): The user in which to burn
+        """
 
         prefix = await self.bot.get_prefix(ctx.message)
+        message = await self.get_message(ctx, prefix, user_to_match)
 
-        async for message in ctx.channel.history(limit=self.SEARCH_LIMIT):
-            if message.author == user_to_match and not message.content.startswith(
-                prefix
-            ):
-                matched_message = message
-                break
-
-        if not matched_message:
-            await ctx.send_deny_embed("I could not a find a message to reply to")
-            return
-
-        for emoji in ["🔥", "🚒", "👨‍🚒"]:
-            await matched_message.add_reaction(emoji)
-
-        message = random.choice(self.PHRASES)
-        embed = BurnEmbed(description=f"🔥🔥🔥 {message} 🔥🔥🔥")
-        await ctx.send(embed=embed, targets=[user_to_match])
+        await self.handle_burn(ctx, user_to_match, message)
--- techsupport_bot/tests/test_burn.py
+++ techsupport_bot/tests/test_burn.py
@@ -1,41 +1,157 @@
-import asyncio
-import imp
 from functools import wraps
+from unittest.mock import AsyncMock
 
 import discord
 import mock
-import munch
 import pytest
-import util
 from discord.ext import commands
+from extensions import Burn
 from mock import patch
 
-# Patch function decorators before importing Burn
-patch("util.with_typing", lambda x: x).start()
-patch("discord.ext.commands.guild_only", return_value=None)
-patch("discord.ext.commands.command", command=lambda f: f).start()
 
-from extensions import Burn, BurnEmbed
+class MockMember:
+    def __init__(self, id=None):
+        self.id = id
 
 
-def test_burnembed() -> None:
-    embed = BurnEmbed(description="test")
-    assert embed.title == "Burn Alert!"
+class MockContext:
+    def __init__(self, channel=None):
+        self.channel = channel
+
+
+class MockChannel:
+    def __init__(self, history=None):
+        self.message_history = history
+
+    async def history(self, limit):
+        for message in self.message_history:
+            yield message
+
+
+class MockMessage:
+    def __init__(self, content=None, author=None):
+        self.content = content
+        self.author = author
+
+
+@mock.patch("asyncio.create_task", return_value=None)
+def test_generate_burn_embed(async_patch):
+    burn = Burn("1")
+    burn.PHRASES = ["Test Phrase"]
+    embed = burn.generate_burn_embed()
     assert embed.color == discord.Color.red()
-    assert embed.description == "test"
+    assert embed.title == "Burn Alert!"
+    assert embed.description == "🔥🔥🔥 Test Phrase 🔥🔥🔥"
 
 
 @pytest.mark.asyncio
 @mock.patch("asyncio.create_task", return_value=None)
-async def test_creation(asyncio_patch) -> None:
-    test = Burn("1")
+async def test_get_message(async_patch):
+    burn = Burn("1")
+    # Setup discord env
+    member_to_burn = MockMember()
+    message_to_burn = MockMessage(content="words", author=member_to_burn)
+    message_history = [message_to_burn]
+    channel = MockChannel(history=message_history)
+    context = MockContext(channel=channel)
 
-# This one needs some work, but I did the most annoying parts
+    returned_message = await burn.get_message(context, ".", member_to_burn)
+    assert returned_message == message_to_burn
+
+
 @pytest.mark.asyncio
 @mock.patch("asyncio.create_task", return_value=None)
-async def test_burn_command(self) -> None:
-    test = Burn("1")
-    ctx = "1"
-    user_to_match = "1"
-    test.burn(ctx, user_to_match=user_to_match)
-    assert test.SEARCH_LIMIT == 50
+async def test_get_message_late_in_list(async_patch):
+    burn = Burn("1")
+    # Setup discord env
+    member_to_burn = MockMember()
+    random_member = MockMember()
+    random_message = MockMessage(content="by someone else", author=random_member)
+    message_to_burn = MockMessage(content="Burned", author=member_to_burn)
+    message_history = [
+        random_message,
+        random_message,
+        random_message,
+        random_message,
+        message_to_burn,
+    ]
+    channel = MockChannel(history=message_history)
+    context = MockContext(channel=channel)
+    returned_message = await burn.get_message(context, ".", member_to_burn)
+    assert returned_message == message_to_burn
+
+
+@pytest.mark.asyncio
+@mock.patch("asyncio.create_task", return_value=None)
+async def test_get_message_only_prefix(async_patch):
+    burn = Burn("1")
+    # Setup discord env
+    member_to_burn = MockMember()
+    message_to_burn = MockMessage(content=".words", author=member_to_burn)
+    message_history = [message_to_burn]
+    channel = MockChannel(history=message_history)
+    context = MockContext(channel=channel)
+
+    returned_message = await burn.get_message(context, ".", member_to_burn)
+    assert returned_message == None
+
+
+@pytest.mark.asyncio
+@mock.patch("asyncio.create_task", return_value=None)
+async def test_get_message_no_burn_messages(async_patch):
+    burn = Burn("1")
+    # Setup discord env
+    member_to_burn = MockMember()
+    random_member = MockMember()
+    random_message = MockMessage(content="by someone else", author=random_member)
+    message_history = [
+        random_message,
+        random_message,
+        random_message,
+        random_message,
+        random_message,
+        random_message,
+        random_message,
+        random_message,
+    ]
+    channel = MockChannel(history=message_history)
+    context = MockContext(channel=channel)
+
+    returned_message = await burn.get_message(context, ".", member_to_burn)
+    assert returned_message == None
+
+
+@pytest.mark.asyncio
+@mock.patch("asyncio.create_task", return_value=None)
+async def test_handle_burn(async_patch):
+    burn = Burn("1")
+    # Setup discord env
+    member_to_burn = MockMember()
+    message_to_burn = MockMessage(content="words", author=member_to_burn)
+    message_history = [message_to_burn]
+    channel = MockChannel(history=message_history)
+    context = MockContext(channel=channel)
+    message_to_burn.add_reaction = AsyncMock()
+    context.send = AsyncMock()
+
+    await burn.handle_burn(context, member_to_burn, message_to_burn)
+    expected_calls = [
+        mock.call("🔥"),
+        mock.call("🚒"),
+        mock.call("👨‍🚒"),
+    ]
+    message_to_burn.add_reaction.assert_has_calls(expected_calls, any_order=True)
+    context.send.assert_called_once()
+
+
+@pytest.mark.asyncio
+@mock.patch("asyncio.create_task", return_value=None)
+async def test_handle_burn_no_message(async_patch):
+    burn = Burn("1")
+    # Setup discord env
+    context = MockContext()
+    context.send_deny_embed = AsyncMock()
+
+    await burn.handle_burn(context, None, None)
+
+    context.send_deny_embed.assert_called_once()
