93ec4e442733b86d4dbd54ab8547a54de621d8f6
Ajax146 <31014239+ajax146@users.noreply.github.com>
2023-05-25 12:39:58 +0000

    Add missing file

---
+++ techsupport_bot/tests/helpers/bot.py
@@ -0,0 +1,15 @@
+"""
+This is a file to store the fake disord.Bot objection
+"""
+
+
+class MockBot:
+    """
+    This is the MockBot class
+
+    Currently implemented variables and methods:
+    id -> An integer containing the ID of the bot
+    """
+
+    def __init__(self, id=None):
+        self.id = id
