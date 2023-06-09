af023997aae04d6fe33793c0634225784e3b45e8
Ajax146 <31014239+ajax146@users.noreply.github.com>
2023-06-09 18:54:41 +0000

    Saved embeds.py

--- techsupport_bot/embeds.py
+++ techsupport_bot/embeds.py
@@ -17,18 +17,3 @@ class ConfirmEmbed(discord.Embed):
         self.title = "😄 👍"
         self.description = message
         self.color = discord.Color.green()
-
-
-class DenyEmbed(discord.Embed):
-    """An embed for deny responses.
-
-    parameters:
-        message (str): the response message
-    """
-
-    def __init__(self, *args, **kwargs):
-        message = kwargs.pop("message")
-        super().__init__(*args, **kwargs)
-        self.title = "😕 👎"
-        self.description = message
-        self.color = discord.Color.red()
