e93cb4de1e18109c27cddb7be6708c194831826a
ajax146 <31014239+ajax146@users.noreply.github.com>
2023-06-11 17:23:35 -0400

    Major unit test refactor (#420)
    Moving mocking of asyncio to config file
    Adds step 1, 2, and 3 comments
    Adds more tests, but reduces the number of asserts per test
    Removes mock and aiounittest
    Updates the pytest workflow

--- techsupport_bot/tests/test_base_auxiliary.py
+++ techsupport_bot/tests/test_base_auxiliary.py
@@ -4,10 +4,9 @@ This contains 15 tests
 """
 
 
-from unittest.mock import AsyncMock
+from unittest.mock import AsyncMock, call
 
 import discord
-import mock
 import pytest
 from base import auxiliary
 from hypothesis import given
@@ -22,96 +21,117 @@ class Test_SearchForMessage:
     @pytest.mark.asyncio
     async def test_searching_only_content(self):
         """Test to ensure that content searching works"""
+        # Step 1 - Setup env
         discord_env = config_for_tests.FakeDiscordEnv()
         discord_env.channel.message_history = [discord_env.message_person2_noprefix_1]
-        assert (
-            await auxiliary.search_channel_for_message(
-                channel=discord_env.channel, content_to_match="message"
-            )
-            == discord_env.message_person2_noprefix_1
+
+        # Step 2 - Call the function
+        message = await auxiliary.search_channel_for_message(
+            channel=discord_env.channel, content_to_match="message"
         )
+
+        # Step 3 - Assert that everything works
+        assert message == discord_env.message_person2_noprefix_1
 
     @pytest.mark.asyncio
     async def test_searching_only_member(self):
         """Test to ensure that member searching works"""
+        # Step 1 - Setup env
         discord_env = config_for_tests.FakeDiscordEnv()
         discord_env.channel.message_history = [discord_env.message_person2_noprefix_1]
-        assert (
-            await auxiliary.search_channel_for_message(
-                channel=discord_env.channel, member_to_match=discord_env.person2
-            )
-            == discord_env.message_person2_noprefix_1
+
+        # Step 2 - Call the function
+        message = await auxiliary.search_channel_for_message(
+            channel=discord_env.channel, member_to_match=discord_env.person2
         )
+
+        # Step 3 - Assert that everything works
+        assert message == discord_env.message_person2_noprefix_1
 
     @pytest.mark.asyncio
     async def test_searching_content_and_member(self):
         """Test to ensure that member and content searching works together"""
+        # Step 1 - Setup env
         discord_env = config_for_tests.FakeDiscordEnv()
         discord_env.channel.message_history = [discord_env.message_person2_noprefix_1]
-        assert (
-            await auxiliary.search_channel_for_message(
-                channel=discord_env.channel,
-                member_to_match=discord_env.person2,
-                content_to_match="message",
-            )
-            == discord_env.message_person2_noprefix_1
+
+        # Step 2 - Call the function
+        message = await auxiliary.search_channel_for_message(
+            channel=discord_env.channel,
+            member_to_match=discord_env.person2,
+            content_to_match="message",
         )
+
+        # Step 3 - Assert that everything works
+        assert message == discord_env.message_person2_noprefix_1
 
     @pytest.mark.asyncio
     async def test_searching_ignore_prefix(self):
         """Test to ensure that a given prefix is ignored"""
+        # Step 1 - Setup env
         discord_env = config_for_tests.FakeDiscordEnv()
         discord_env.channel.message_history = [discord_env.message_person1_prefix]
-        assert (
-            await auxiliary.search_channel_for_message(
-                channel=discord_env.channel,
-                prefix=config_for_tests.PREFIX,
-                allow_bot=False,
-            )
-            is None
+
+        # Step 2 - Call the function
+        message = await auxiliary.search_channel_for_message(
+            channel=discord_env.channel,
+            prefix=config_for_tests.PREFIX,
+            allow_bot=False,
         )
+
+        # Step 3 - Assert that everything works
+        assert message is None
 
     @pytest.mark.asyncio
     async def test_searching_keep_prefix(self):
         """Test to ensure that a given prefix is found"""
+        # Step 1 - Setup env
         discord_env = config_for_tests.FakeDiscordEnv()
         discord_env.channel.message_history = [discord_env.message_person1_prefix]
-        assert (
-            await auxiliary.search_channel_for_message(
-                channel=discord_env.channel, allow_bot=False
-            )
-            == discord_env.message_person1_prefix
+
+        # Step 2 - Call the function
+        message = await auxiliary.search_channel_for_message(
+            channel=discord_env.channel, allow_bot=False
         )
+
+        # Step 3 - Assert that everything works
+        assert message == discord_env.message_person1_prefix
 
     @pytest.mark.asyncio
     async def test_searching_ignores_bot(self):
         """Test to ensure that bot messages are ignored"""
+        # Step 1 - Setup env
         discord_env = config_for_tests.FakeDiscordEnv()
         discord_env.channel.message_history = [discord_env.message_person3_noprefix]
-        assert (
-            await auxiliary.search_channel_for_message(
-                channel=discord_env.channel, allow_bot=False
-            )
-            is None
+
+        # Step 2 - Call the function
+        message = await auxiliary.search_channel_for_message(
+            channel=discord_env.channel, allow_bot=False
         )
+
+        # Step 3 - Assert that everything works
+        assert message is None
 
     @pytest.mark.asyncio
     async def test_searching_finds_bot(self):
         """Test to ensure that bot messages are found"""
+        # Step 1 - Setup env
         discord_env = config_for_tests.FakeDiscordEnv()
         discord_env.channel.message_history = [discord_env.message_person3_noprefix]
-        assert (
-            await auxiliary.search_channel_for_message(
-                channel=discord_env.channel, allow_bot=True
-            )
-            == discord_env.message_person3_noprefix
+
+        # Step 2 - Call the function
+        message = await auxiliary.search_channel_for_message(
+            channel=discord_env.channel, allow_bot=True
         )
+
+        # Step 3 - Assert that everything works
+        assert message == discord_env.message_person3_noprefix
 
     @pytest.mark.asyncio
     async def test_searching_member_multiple_messages(self):
         """Test to ensure that the most recent message is picked, if multiple match the critera"""
+        # Step 1 - Setup env
         discord_env = config_for_tests.FakeDiscordEnv()
-
         message_history = [
             discord_env.message_person1_noprefix_1,
             discord_env.message_person2_noprefix_2,
@@ -122,18 +142,19 @@ class Test_SearchForMessage:
         ]
         discord_env.channel.message_history = message_history
 
-        assert (
-            await auxiliary.search_channel_for_message(
-                channel=discord_env.channel, member_to_match=discord_env.person2
-            )
-            == discord_env.message_person2_noprefix_2
+        # Step 2 - Call the function
+        message = await auxiliary.search_channel_for_message(
+            channel=discord_env.channel, member_to_match=discord_env.person2
         )
+
+        # Step 3 - Assert that everything works
+        assert message == discord_env.message_person2_noprefix_2
 
     @pytest.mark.asyncio
     async def test_searching_by_member_not_first_message(self):
         """Test to ensure that the first message is not always picked"""
+        # Step 1 - Setup env
         discord_env = config_for_tests.FakeDiscordEnv()
-
         message_history = [
             discord_env.message_person1_noprefix_1,
             discord_env.message_person1_noprefix_1,
@@ -142,18 +163,19 @@ class Test_SearchForMessage:
         ]
         discord_env.channel.message_history = message_history
 
-        assert (
-            await auxiliary.search_channel_for_message(
-                channel=discord_env.channel, member_to_match=discord_env.person2
-            )
-            == discord_env.message_person2_noprefix_2
+        # Step 2 - Call the function
+        message = await auxiliary.search_channel_for_message(
+            channel=discord_env.channel, member_to_match=discord_env.person2
         )
+
+        # Step 3 - Assert that everything works
+        assert message == discord_env.message_person2_noprefix_2
 
     @pytest.mark.asyncio
     async def test_searching_by_nothing_returns_first_message(self):
         """Test to ensure that searching with no critera will always return the first message"""
+        # Step 1 - Setup env
         discord_env = config_for_tests.FakeDiscordEnv()
-
         message_history = [
             discord_env.message_person1_prefix,
             discord_env.message_person2_noprefix_1,
@@ -164,27 +186,32 @@ class Test_SearchForMessage:
         ]
         discord_env.channel.message_history = message_history
 
-        assert (
-            await auxiliary.search_channel_for_message(channel=discord_env.channel)
-            == discord_env.message_person1_prefix
+        # Step 2 - Call the function
+        message = await auxiliary.search_channel_for_message(
+            channel=discord_env.channel
         )
+        # Step 3 - Assert that everything works
+        assert message == discord_env.message_person1_prefix
 
     @pytest.mark.asyncio
     @given(config_for_tests.rand_history())
     async def test_find_message_random_history(self, given_history):
         """Test to ensure that given a random history,
         the find message functions always works as expected"""
+        # Step 1 - Setup env
         discord_env = config_for_tests.FakeDiscordEnv()
         discord_env.channel.message_history = given_history
+
+        # Step 2 - Call the function
         found_message = await auxiliary.search_channel_for_message(
             channel=discord_env.channel,
             prefix=config_for_tests.PREFIX,
             content_to_match="a",
             allow_bot=False,
         )
-        if found_message is None:
-            assert found_message is None
-        else:
+
+        # Step 3 - Assert that everything works
+        if found_message is not None:
             assert found_message.author.bot is False
             assert "a" in found_message.content
             assert not found_message.content.startswith(config_for_tests.PREFIX)
@@ -196,21 +223,27 @@ class Test_GenerateBasicEmbed:
     @given(text(), text())
     def test_generate_embed(self, title, description):
         """Property test to ensure that embeds are generated correctly"""
+        # Step 2 - Call the function
         embed = auxiliary.generate_basic_embed(
             title=title, description=description, color=discord.Color.random()
         )
+
+        # Step 3 - Assert that everything works
         assert embed.title == title
         assert embed.description == description
         assert isinstance(embed.color, discord.Color)
 
     def test_generate_embed_with_url(self):
         """Test to ensure that the URL property is added correctly"""
+        # Step 2 - Call the function
         embed = auxiliary.generate_basic_embed(
             title="A",
             description="A",
             color=discord.Color.random(),
             url="https://a.com",
         )
+
+        # Step 3 - Assert that everything works
         assert embed.thumbnail.url == "https://a.com"
 
 
@@ -220,31 +253,39 @@ class Test_AddReactions:
     @pytest.mark.asyncio
     async def test_with_one_reaction(self):
         """Test add_list_of_reactions with just 1 emoji"""
-        with mock.patch("asyncio.create_task", return_value=None):
-            discord_env = config_for_tests.FakeDiscordEnv()
-            discord_env.message_person1_noprefix_1.add_reaction = AsyncMock()
-            await auxiliary.add_list_of_reactions(
-                message=discord_env.message_person1_noprefix_1, reactions=["🔥"]
-            )
-            discord_env.message_person1_noprefix_1.add_reaction.assert_awaited_once_with(
-                "🔥"
-            )
+        # Step 1 - Setup env
+        discord_env = config_for_tests.FakeDiscordEnv()
+        discord_env.message_person1_noprefix_1.add_reaction = AsyncMock()
+
+        # Step 2 - Call the function
+        await auxiliary.add_list_of_reactions(
+            message=discord_env.message_person1_noprefix_1, reactions=["🔥"]
+        )
+
+        # Step 3 - Assert that everything works
+        discord_env.message_person1_noprefix_1.add_reaction.assert_awaited_once_with(
+            "🔥"
+        )
 
     @pytest.mark.asyncio
     async def test_with_many_reaction(self):
         """Test add_list_of_reactions with just amny emoji"""
-        with mock.patch("asyncio.create_task", return_value=None):
-            discord_env = config_for_tests.FakeDiscordEnv()
-            discord_env.message_person1_noprefix_1.add_reaction = AsyncMock()
-            await auxiliary.add_list_of_reactions(
-                message=discord_env.message_person1_noprefix_1,
-                reactions=["🔥", "⬅️", "🗑️"],
-            )
-            expected_calls = [
-                mock.call("🔥"),
-                mock.call("⬅️"),
-                mock.call("🗑️"),
-            ]
+        # Step 1 - Setup env
+        discord_env = config_for_tests.FakeDiscordEnv()
+        discord_env.message_person1_noprefix_1.add_reaction = AsyncMock()
+
+        # Step 2 - Call the function
+        await auxiliary.add_list_of_reactions(
+            message=discord_env.message_person1_noprefix_1,
+            reactions=["🔥", "⬅️", "🗑️"],
+        )
+
+        # Step 3 - Assert that everything works
+        expected_calls = [
+            call("🔥"),
+            call("⬅️"),
+            call("🗑️"),
+        ]
         discord_env.message_person1_noprefix_1.add_reaction.assert_has_calls(
             expected_calls, any_order=False
         )
--- techsupport_bot/tests/test_extensions_burn.py
+++ techsupport_bot/tests/test_extensions_burn.py
@@ -1,11 +1,10 @@
 """
 This is a file to test the extensions/burn.py file
-This contains 3 tests
+This contains 6 tests
 """
 
 from unittest.mock import AsyncMock
 
-import mock
 import pytest
 from base import auxiliary
 
@@ -15,17 +14,20 @@ from . import config_for_tests
 class Test_Phrases:
     """A simple set of tests to ensure the PHRASES variable won't cause any problems"""
 
-    @mock.patch("asyncio.create_task", return_value=None)
-    def test_all_phrases_are_short(self, _):
+    def test_all_phrases_are_short(self):
         """
         This is a test to ensure that the generate burn embed function is working correctly
         This specifically looks at the description for every phrase in the PHRASES array
         This looks at the length of the description as well to ensure that
             the phrases aren't too long
         """
+        # Step 1 - Setup env
         discord_env = config_for_tests.FakeDiscordEnv()
 
+        # Step 2 - Call the function
         test_phrases = discord_env.burn.PHRASES
+
+        # Step 3 - Assert that everything works
         for phrase in test_phrases:
             assert len(f"🔥🔥🔥 {phrase} 🔥🔥🔥") <= 4096
 
@@ -34,45 +36,112 @@ class Test_HandleBurn:
     """A set of test cases testing the handle_burn function"""
 
     @pytest.mark.asyncio
-    @mock.patch("asyncio.create_task", return_value=None)
-    async def test_handle_burn(self, _):
+    async def test_handle_burn_calls_reactions(self):
         """
         This is a test to ensure that handle_burn works correctly when a valid message can be found
-        It checks to ensure that the reactions are added correctly,
-            and that the send function was called
+        It checks to ensure that the reactions are added correctly
         """
+        # Step 1 - Setup env
         discord_env = config_for_tests.FakeDiscordEnv()
-
         message_history = [discord_env.message_person1_noprefix_1]
         discord_env.channel.message_history = message_history
         discord_env.context.send = AsyncMock()
-
         auxiliary.add_list_of_reactions = AsyncMock()
 
+        # Step 2 - Call the function
         await discord_env.burn.handle_burn(
             discord_env.context,
             discord_env.person1,
             discord_env.message_person1_noprefix_1,
         )
 
+        # Step 3 - Assert that everything works
         auxiliary.add_list_of_reactions.assert_called_once_with(
-            message=discord_env.message_person1_noprefix_1, reactions=["🔥", "🚒", "👨‍🚒"]
+            message=discord_env.message_person1_noprefix_1,
+            reactions=["🔥", "🚒", "👨‍🚒"],
         )
 
+    @pytest.mark.asyncio
+    async def test_handle_burn_calls_send(self):
+        """
+        This is a test to ensure that handle_burn works correctly when a valid message can be found
+        It checks to ensure that the reactions are added correctly
+        """
+        # Step 1 - Setup env
+        discord_env = config_for_tests.FakeDiscordEnv()
+        message_history = [discord_env.message_person1_noprefix_1]
+        discord_env.channel.message_history = message_history
+        discord_env.context.send = AsyncMock()
+        auxiliary.add_list_of_reactions = AsyncMock()
+
+        # Step 2 - Call the function
+        await discord_env.burn.handle_burn(
+            discord_env.context,
+            discord_env.person1,
+            discord_env.message_person1_noprefix_1,
+        )
+
+        # Step 3 - Assert that everything works
         discord_env.context.send.assert_called_once()
 
     @pytest.mark.asyncio
-    @mock.patch("asyncio.create_task", return_value=None)
-    async def test_handle_burn_no_message(self, _):
+    async def test_handle_burn_no_message(self):
         """
         This is a test to ensure that the send_deny_embed
             function is called if no message can be found
         """
+        # Step 1 - Setup env
         discord_env = config_for_tests.FakeDiscordEnv()
-
         discord_env.context.send_deny_embed = AsyncMock()
 
+        # Step 2 - Call the function
         await discord_env.burn.handle_burn(discord_env.context, None, None)
+
+        # Step 3 - Assert that everything works
         discord_env.context.send_deny_embed.assert_called_once_with(
             "I could not a find a message to reply to"
+        )
+
+
+class Test_BurnCommand:
+    """A set of test cases for testing the burn_command function"""
+
+    @pytest.mark.asyncio
+    async def test_calls_search_channel(self):
+        """A simple test to ensure that burn_command calls search_channel_for_message
+        with the correct args"""
+        # Step 1 - Setup env
+        discord_env = config_for_tests.FakeDiscordEnv()
+        auxiliary.search_channel_for_message = AsyncMock()
+        discord_env.burn.handle_burn = AsyncMock()
+
+        # Step 2 - Call the function
+        await discord_env.burn.burn_command(discord_env.context, discord_env.person1)
+
+        # Step 3 - Assert that everything works
+        auxiliary.search_channel_for_message.assert_called_once_with(
+            channel=discord_env.context.channel,
+            prefix=config_for_tests.PREFIX,
+            member_to_match=discord_env.person1,
+        )
+
+    @pytest.mark.asyncio
+    async def test_calls_handle_burn(self):
+        """A simple test to ensure that burn_command calls handle_burn
+        with the correct args"""
+        # Step 1 - Setup env
+        discord_env = config_for_tests.FakeDiscordEnv()
+        auxiliary.search_channel_for_message = AsyncMock(
+            return_value=discord_env.message_person1_noprefix_1
+        )
+        discord_env.burn.handle_burn = AsyncMock()
+
+        # Step 2 - Call the function
+        await discord_env.burn.burn_command(discord_env.context, discord_env.person1)
+
+        # Step 3 - Assert that everything works
+        discord_env.burn.handle_burn.assert_called_once_with(
+            discord_env.context,
+            discord_env.person1,
+            discord_env.message_person1_noprefix_1,
         )
