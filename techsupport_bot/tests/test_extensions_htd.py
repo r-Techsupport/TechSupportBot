55cc111aca821c17498221f3407292d7ede7f98b
Ajax146 <31014239+ajax146@users.noreply.github.com>
2023-06-12 19:10:53 +0000

    Add convert_value_to_integer tests

--- techsupport_bot/tests/test_extensions_htd.py
+++ techsupport_bot/tests/test_extensions_htd.py
@@ -73,3 +73,83 @@ class Test_SplitNicely:
 
         # Step 3 - Assert that everything works
         assert output == ['3276856238658273658724658247658245']
+
+class Test_ConvertToInt:
+    """Tests for convert_value_to_integer"""
+
+    def test_simple_hexadecimal(self):
+        """Test that a simple hex value is properly converted"""
+        # Step 1 - Setup env
+        hextodec = setup_local_extension()
+
+        # Step 2 - Call the function
+        output = hextodec.convert_value_to_integer("0x5")
+
+        # Step 3 - Assert that everything works
+        assert output == 5
+    
+    def test_complex_hexadecimal(self):
+        """Test that a complex hex value is properly converted"""
+        # Step 1 - Setup env
+        hextodec = setup_local_extension()
+
+        # Step 2 - Call the function
+        output = hextodec.convert_value_to_integer("0xFBA34")
+
+        # Step 3 - Assert that everything works
+        assert output == 1030708
+    
+    def test_simple_binary(self):
+        """Test that a simple binary value is properly converted"""
+        # Step 1 - Setup env
+        hextodec = setup_local_extension()
+
+        # Step 2 - Call the function
+        output = hextodec.convert_value_to_integer("0b1")
+
+        # Step 3 - Assert that everything works
+        assert output == 1
+    
+    def test_complex_binary(self):
+        """Test that a complex binary value is properly converted"""
+        # Step 1 - Setup env
+        hextodec = setup_local_extension()
+
+        # Step 2 - Call the function
+        output = hextodec.convert_value_to_integer("0b10101010100110")
+
+        # Step 3 - Assert that everything works
+        assert output == 10918
+    
+    def test_simple_decimal(self):
+        """Test that a simple deciaml value is properly converted"""
+        # Step 1 - Setup env
+        hextodec = setup_local_extension()
+
+        # Step 2 - Call the function
+        output = hextodec.convert_value_to_integer("8")
+
+        # Step 3 - Assert that everything works
+        assert output == 8
+    
+    def test_complex_decimal(self):
+        """Test that a complex deciaml value is properly converted"""
+        # Step 1 - Setup env
+        hextodec = setup_local_extension()
+
+        # Step 2 - Call the function
+        output = hextodec.convert_value_to_integer("58934275971834685")
+
+        # Step 3 - Assert that everything works
+        assert output == 58934275971834685
+    
+    def test_float_handling(self):
+        """Test that a float is turned into an integer"""
+        # Step 1 - Setup env
+        hextodec = setup_local_extension()
+
+        # Step 2 - Call the function
+        output = hextodec.convert_value_to_integer("1.2")
+
+        # Step 3 - Assert that everything works
+        assert output == 1
