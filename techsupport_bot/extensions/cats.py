5b859cac209ae161c856d6ec9e17872f2992f1b5
ajax146 <31014239+ajax146@users.noreply.github.com>
2023-08-29 09:48:28 -0400

    Add cat extension

---
+++ techsupport_bot/extensions/cats.py
@@ -0,0 +1,29 @@
+"""Module for the urban dictionary extension for the discord bot."""
+import base
+import util
+from discord.ext import commands
+
+
+async def setup(bot):
+    """Loading the Cats plugin"""
+    await bot.add_cog(Cats(bot=bot))
+
+
+class Cats(base.BaseCog):
+    """The class for the cat api"""
+
+    API_URL = "https://api.thecatapi.com/v1/images/search?limit=1&api_key={}"
+
+    @util.with_typing
+    @commands.command(name="cat", brief="Gets a cat")
+    async def cat(self, ctx):
+        """Prints a cat to discord
+
+        Args:
+            ctx (commands.Context): The context in which the command was run
+        """
+        url = self.API_URL.format(
+            self.bot.file_config.api.api_keys.cat,
+        )
+        response = await self.bot.http_call("get", url)
+        await ctx.send(response[0].url)
