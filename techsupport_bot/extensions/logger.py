1093ee84a209a781b8c69d9ee282cde2d2fcd5e0
ajax146 <31014239+ajax146@users.noreply.github.com>
2023-10-18 11:29:39 -0400

    Improve logger (#717)

--- techsupport_bot/extensions/logger.py
+++ techsupport_bot/extensions/logger.py
@@ -2,8 +2,10 @@
 import datetime
 
 import discord
+import munch
 from base import cogs
 from botlogging import LogContext, LogLevel
+from discord.ext import commands
 
 
 async def setup(bot):
@@ -19,54 +21,6 @@ async def setup(bot):
 
     await bot.add_cog(Logger(bot=bot, extension_name="logger"))
     bot.add_extension_config("logger", config)
-
-
-class LogEmbed(discord.Embed):
-    """Class to set up logging embed for discord bot."""
-
-    def __init__(self, *args, **kwargs):
-        ctx = kwargs.pop("context")
-        super().__init__(*args, **kwargs)
-
-        content = ctx.message.content[:1024] if ctx.message.content else "None"
-        self.add_field(name="Content", value=content, inline=False)
-        if len(ctx.message.content) > 1024:
-            content1 = ctx.message.content[1025:2048] if ctx.message.content else "None"
-            self.add_field(name="Content(Continue)", value=content1, inline=False)
-        if len(ctx.message.content) > 2048:
-            content2 = ctx.message.content[2049:3072] if ctx.message.content else "None"
-            self.add_field(name="Content(Continue)", value=content2, inline=False)
-        if len(ctx.message.content) > 3072:
-            content3 = ctx.message.content[3073:4096] if ctx.message.content else "None"
-            self.add_field(name="Content(Continue)", value=content3, inline=False)
-
-        self.add_field(
-            name="Channel",
-            value=f"{ctx.channel.name} ({ctx.channel.mention})" or "Unknown",
-        )
-        self.add_field(name="Display Name", value=ctx.author.display_name or "Unknown")
-        if ctx.author.nick:
-            self.add_field(
-                name="Global Name", value=ctx.author.global_name or "Unknown"
-            )
-        self.add_field(name="Name", value=ctx.author.name or "Unknown")
-        if ctx.author.discriminator != "0":
-            self.add_field(
-                name="Discriminator", value=ctx.author.discriminator or "None"
-            )
-        self.add_field(
-            name="Roles",
-            value=",".join([role.name for role in ctx.author.roles[1:]]) or "None",
-            inline=False,
-        )
-
-        self.set_thumbnail(url=ctx.author.display_avatar.url)
-        self.color = discord.Color.greyple()
-
-        self.timestamp = datetime.datetime.utcnow()
-        self.set_footer(
-            text=f"Author ID: {ctx.author.id} • Message ID: {ctx.message.id}"
-        )
 
 
 class Logger(cogs.MatchCog):
@@ -85,23 +39,21 @@ class Logger(cogs.MatchCog):
                 return False
         return True
 
-    async def response(self, config, ctx, _, __):
+    async def response(self, config: munch.Munch, ctx: commands.Context, _, __) -> None:
         """Method to generate the response from the logger."""
-        if isinstance(ctx.channel, discord.Thread):
-            mapped_id = config.extensions.logger.channel_map.value.get(
-                str(ctx.channel.parent_id)
-            )
-        else:
-            mapped_id = config.extensions.logger.channel_map.value.get(
-                str(ctx.channel.id)
-            )
+        # Get the ID of the channel, or parent channel in the case of threads
+        mapped_id = config.extensions.logger.channel_map.value.get(
+            str(getattr(ctx.channel, "parent_id", ctx.channel.id))
+        )
         if not mapped_id:
             return
 
+        # Get the channel object associated with the ID
         channel = ctx.guild.get_channel(int(mapped_id))
         if not channel:
             return
 
+        # Don't log stuff cross-guild
         if channel.guild.id != ctx.guild.id:
             config = await self.bot.get_context_config(ctx)
             log_channel = config.get("logging_channel")
@@ -113,6 +65,109 @@ class Logger(cogs.MatchCog):
             )
             return
 
+        # Ensure we have attachments re-uploaded
+        attachments = await self.build_attachments(ctx, config)
+
+        # Add avatar to attachments to all it to be added to the embed
+        attachments.insert(
+            0, await ctx.author.display_avatar.to_file(filename="avatar.png")
+        )
+
+        # Make and send the embed and files
+        embed = self.build_embed(ctx)
+        await channel.send(embed=embed, files=attachments[:11])
+
+    def build_embed(self, ctx: commands.Context) -> discord.Embed:
+        """Builds the logged messag embed
+
+        Args:
+            ctx (commands.Context): The context that the message to log was sent in
+
+        Returns:
+            discord.Embed: The prepared embed ready to send to the log channel
+        """
+        embed = discord.Embed()
+
+        # Set basic items
+        embed.color = discord.Color.greyple()
+        embed.timestamp = datetime.datetime.utcnow()
+
+        # Add the message content
+        embed.title = "Content"
+        embed.description = getattr(ctx.message, "clean_content", "No content")
+        if len(embed.description) == 0:
+            embed.description = "No content"
+
+        # Add the channel/thread name
+        main_channel = getattr(ctx.channel, "parent", ctx.channel)
+        embed.add_field(
+            name="Channel",
+            value=f"{main_channel.name} ({main_channel.mention})",
+        )
+        if isinstance(ctx.channel, discord.Thread):
+            embed.add_field(
+                name="Thread",
+                value=f"{ctx.channel.name} ({ctx.channel.mention})",
+            )
+
+        # Add username, display name, and nickname
+        embed.add_field(
+            name="Display Name", value=getattr(ctx.author, "display_name", "Unknown")
+        )
+        if getattr(ctx.author, "nick", False):
+            embed.add_field(
+                name="Global Name", value=getattr(ctx.author, "global_name", "Unknown")
+            )
+        embed.add_field(name="Name", value=getattr(ctx.author, "name", "Unknown"))
+
+        # Add roles
+        embed.add_field(
+            name="Roles",
+            value=", ".join(self.generate_role_list(ctx.author)),
+        )
+
+        # Add avatar
+        embed.set_thumbnail(url="attachment://avatar.png")
+
+        # Add footer with IDs for better searchings
+        embed.set_footer(
+            text=f"Author ID: {ctx.author.id} • Message ID: {ctx.message.id}"
+        )
+
+        return embed
+
+    def generate_role_list(self, author: discord.Member) -> list[str]:
+        """Makes a list of role names from the passed member
+
+        Args:
+            author (discord.Member): The member to get roles from
+
+        Returns:
+            list[str]: The list of roles, highest role first
+        """
+        if not hasattr(author, "roles"):
+            return ["None"]
+
+        roles = [role.name for role in author.roles[1:]]
+        roles.reverse()
+
+        if len(roles) == 0:
+            roles = ["None"]
+
+        return roles
+
+    async def build_attachments(
+        self, ctx: commands.Context, config: munch.Munch
+    ) -> list[discord.File]:
+        """Reuploads and builds a list of attachments to send along side the embed
+
+        Args:
+            ctx (commands.Context): The context the original message was sent in
+            config (munch.Munch): The config from the guild
+
+        Returns:
+            list[discord.File]: The list of file objects ready to be sent
+        """
         attachments: list[discord.File] = []
         if ctx.message.attachments:
             total_attachment_size = 0
@@ -125,11 +180,11 @@ class Logger(cogs.MatchCog):
                 log_channel = config.get("logging_channel")
                 await self.bot.logger.send_log(
                     message=(
-                        f"Logger did not reupload {lf} file(s) due to file size limit."
+                        f"Logger did not reupload {lf} file(s) due to file size limit"
+                        f" on message {ctx.message.id} in channel {ctx.channel.name}."
                     ),
                     level=LogLevel.INFO,
                     channel=log_channel,
                     context=LogContext(guild=ctx.guild, channel=ctx.channel),
                 )
-
-        await channel.send(embed=LogEmbed(context=ctx), files=attachments[:10])
+        return attachments
