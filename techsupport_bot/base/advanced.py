352d61108e36a2d05463404ecd7581544268cfd5
ajax146 <31014239+ajax146@users.noreply.github.com>
2023-11-10 21:14:18 -0500

    Move nickname to a function file (#748)
    Moves nickname filtering away from base/advanced.py and into funcitons/nickname.py
    This also refactors the code a bit to remove some indentation

--- techsupport_bot/base/advanced.py
+++ techsupport_bot/base/advanced.py
@@ -3,9 +3,6 @@
 import asyncio
 import datetime
 import json
-import random
-import re
-import string
 
 import discord
 import error as custom_errors
@@ -15,7 +12,6 @@ from base import auxiliary, data
 from botlogging import LogContext, LogLevel
 from discord import app_commands
 from discord.ext import commands
-from unidecode import unidecode
 
 
 class AdvancedBot(data.DataBot):
@@ -337,71 +333,6 @@ class AdvancedBot(data.DataBot):
             message="Bot online", level=LogLevel.INFO, console_only=True
         )
         await self.get_owner()
-
-    def format_username(self, username: str) -> str:
-        """Formats a username to be all ascii and easily readable and pingable
-
-        Args:
-            username (str): The original users username
-
-        Returns:
-            str: The new username with all formatting applied
-        """
-
-        # Prepare a random string, just in case
-        random_string = "".join(random.choice(string.ascii_letters) for _ in range(10))
-
-        # Step 1 - Force all ascii
-        username = unidecode(username)
-
-        # Step 2 - Remove all markdown
-        markdown_pattern = r"(\*\*|__|\*|_|\~\~|`|#+|-{3,}|\|{3,}|>)"
-        username = re.sub(markdown_pattern, "", username)
-
-        # Step 3 - Strip
-        username = username.strip()
-
-        # Step 4 - Fix dumb spaces
-        username = re.sub(r"\s+", " ", username)
-        username = re.sub(r"(\b\w) ", r"\1", username)
-
-        # Step 5 - Start with letter
-        match = re.search(r"[A-Za-z]", username)
-        if match:
-            username = username[match.start() :]
-        else:
-            username = ""
-
-        # Step 6 - Length check
-        if len(username) < 3 and len(username) > 0:
-            username = f"{username}-USER-{random_string}"
-        elif len(username) == 0:
-            username = f"USER-{random_string}"
-        username = username[:32]
-
-        return username
-
-    async def on_member_join(self, member: discord.Member) -> None:
-        """See: https://discordpy.readthedocs.io/en/latest/api.html#discord.on_member_join"""
-        config = self.guild_configs[str(member.guild.id)]
-
-        if config.get("nickname_filter", False):
-            temp_name = self.format_username(member.display_name)
-            if temp_name != member.display_name:
-                await member.edit(nick=temp_name)
-                try:
-                    await member.send(
-                        "Your nickname has been changed to make it easy to read and"
-                        f" ping your name. Your new nickname is {temp_name}."
-                    )
-                except discord.Forbidden:
-                    channel = config.get("logging_channel")
-                    await self.logger.send_log(
-                        message=f"Could not DM {member.name} about nickname changes",
-                        level=LogLevel.WARNING,
-                        channel=channel,
-                        context=LogContext(guild=member.guild),
-                    )
 
     async def on_app_command_error(
         self,
---
+++ techsupport_bot/functions/nickname.py
@@ -0,0 +1,101 @@
+"""
+A listener which changes peoples nicknames on join
+The cog in the file is named:
+    AutoNickName
+
+This file does not contain any commands
+"""
+
+import random
+import re
+import string
+
+import discord
+from base import cogs
+from botlogging import LogContext, LogLevel
+from discord.ext import commands
+from unidecode import unidecode
+
+
+async def setup(bot):
+    """Registers the AutoNickName Cog"""
+    await bot.add_cog(AutoNickName(bot=bot))
+
+
+class AutoNickName(cogs.BaseCog):
+    """
+    The class that holds the listener and functions to auto change peoples nicknames
+    """
+
+    def format_username(self, username: str) -> str:
+        """Formats a username to be all ascii and easily readable and pingable
+
+        Args:
+            username (str): The original users username
+
+        Returns:
+            str: The new username with all formatting applied
+        """
+
+        # Prepare a random string, just in case
+        random_string = "".join(random.choice(string.ascii_letters) for _ in range(10))
+
+        # Step 1 - Force all ascii
+        username = unidecode(username)
+
+        # Step 2 - Remove all markdown
+        markdown_pattern = r"(\*\*|__|\*|_|\~\~|`|#+|-{3,}|\|{3,}|>)"
+        username = re.sub(markdown_pattern, "", username)
+
+        # Step 3 - Strip
+        username = username.strip()
+
+        # Step 4 - Fix dumb spaces
+        username = re.sub(r"\s+", " ", username)
+        username = re.sub(r"(\b\w) ", r"\1", username)
+
+        # Step 5 - Start with letter
+        match = re.search(r"[A-Za-z]", username)
+        if match:
+            username = username[match.start() :]
+        else:
+            username = ""
+
+        # Step 6 - Length check
+        if len(username) < 3 and len(username) > 0:
+            username = f"{username}-USER-{random_string}"
+        elif len(username) == 0:
+            username = f"USER-{random_string}"
+        username = username[:32]
+
+        return username
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member: discord.Member) -> None:
+        """See: https://discordpy.readthedocs.io/en/latest/api.html#discord.on_member_join"""
+        config = self.bot.guild_configs[str(member.guild.id)]
+
+        # Don't do anything if the filter is off for the guild
+        if not config.get("nickname_filter", False):
+            return
+
+        modified_name = self.format_username(member.display_name)
+
+        # If the name didn't change for the user, do nothing
+        if modified_name == member.display_name:
+            return
+
+        await member.edit(nick=modified_name)
+        try:
+            await member.send(
+                "Your nickname has been changed to make it easy to read and"
+                f" ping your name. Your new nickname is {modified_name}."
+            )
+        except discord.Forbidden:
+            channel = config.get("logging_channel")
+            await self.bot.logger.send_log(
+                message=f"Could not DM {member.name} about nickname changes",
+                level=LogLevel.WARNING,
+                channel=channel,
+                context=LogContext(guild=member.guild),
+            )
