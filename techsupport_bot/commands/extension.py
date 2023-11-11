2efbc448dde9515af519f842d6d7303030971e51
ajax146 <31014239+ajax146@users.noreply.github.com>
2023-11-10 19:50:48 -0500

    Fixes the auto help prompts

--- techsupport_bot/commands/extension.py
+++ techsupport_bot/commands/extension.py
@@ -12,7 +12,7 @@ This file contains 4 commands:
 
 import discord
 import ui
-from base import auxiliary, cogs
+from base import auxiliary, cogs, extension
 from discord.ext import commands
 
 
@@ -41,58 +41,7 @@ class ExtensionControl(cogs.BaseCog):
         """
 
         # Executed if there are no/invalid args supplied
-        def get_help_embed(self, command_prefix):
-            # Gets commands, checks if first supplied arg is valid
-            embed = discord.Embed(
-                title="Incorrect/no args provided, correct command usage:"
-            )
-
-            # Loops through each command in this cog
-            for command in self.bot.get_cog(self.qualified_name).walk_commands():
-                if issubclass(command.__class__, commands.Group):
-                    continue
-
-                if command.full_parent_name == "":
-                    syntax = f"{command_prefix}{command.name}"
-
-                else:
-                    syntax = (
-                        f"{command_prefix}{command.full_parent_name} {command.name}"
-                    )
-
-                embed.color = discord.Color.green()
-                embed.add_field(
-                    name=f"`{syntax} {command.usage or ''}`",
-                    value=command.description or "No description available",
-                    inline=False,
-                )
-
-            return embed
-
-        # Checks if no arguments were supplied
-        if len(ctx.message.content.split()) < 2:
-            await ctx.send(
-                embed=get_help_embed(self, await self.bot.get_prefix(ctx.message))
-            )
-
-        # Checks whether the first given argument is valid if more than one argument is supplied
-        elif ctx.message.content.split().pop(1) not in [
-            command.name
-            for command in self.bot.get_cog(self.qualified_name).walk_commands()
-        ]:
-            view = ui.Confirm()
-            await view.send(
-                message="Invalid argument! Show help command?",
-                channel=ctx.channel,
-                author=ctx.author,
-                timeout=10,
-            )
-            await view.wait()
-            if view.value != ui.ConfirmResponse.CONFIRMED:
-                return
-            await ctx.send(
-                embed=get_help_embed(self, await self.bot.get_prefix(ctx.message))
-            )
+        await extension.extension_help(self, ctx, self.__module__[9:])
 
     @auxiliary.with_typing
     @extension_group.command(
