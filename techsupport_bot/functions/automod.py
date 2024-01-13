5007b22f6b914c2957dbfd1ed944efb76e3c0a2a
ajax146 <31014239+ajax146@users.noreply.github.com>
2024-01-13 16:11:04 -0500

    Formatting

--- techsupport_bot/functions/automod.py
+++ techsupport_bot/functions/automod.py
@@ -24,28 +24,34 @@ async def setup(bot: bot.TechSupportBot) -> None:
 class AutoMod(cogs.MatchCog):
     async def match(self, config, ctx, content):
         ...
-    
+
     async def response(self, config, ctx, content, _):
         ...
-    
 
-    async def run_all_checks(self, message: discord.Message) -> bool:
-        ...
-    
-    async def run_only_string_checks(self, content: str, member: discord.Member) -> bool:
-        ...
-    
-    async def handle_file_extensions(self, attachments: list[discord.Attachment]) -> bool:
-        ...
-    
-    async def handle_mentions(self, message: discord.Message) -> bool:
-        ...
-    
-    async def handle_exact_string(self, content: str) -> bool:
-        ...
-    
-    async def handle_regex_string(self, content: str) -> bool:
-        ...
 
-    async def should_ban_or_warn(self, member: discord.Member):
-        ...
+async def run_all_checks(message: discord.Message) -> bool:
+    ...
+
+
+async def run_only_string_checks(content: str, member: discord.Member) -> bool:
+    ...
+
+
+async def handle_file_extensions(attachments: list[discord.Attachment]) -> bool:
+    ...
+
+
+async def handle_mentions(message: discord.Message) -> bool:
+    ...
+
+
+async def handle_exact_string(content: str) -> bool:
+    ...
+
+
+async def handle_regex_string(content: str) -> bool:
+    ...
+
+
+async def should_ban_or_warn(member: discord.Member):
+    ...
