ad149fa3da536c3a581d6fb981be22fedbaba8fd
ajax146 <31014239+ajax146@users.noreply.github.com>
2026-04-14 13:38:16 -0700

    Formatting

--- techsupport_bot/functions/logger.py
+++ techsupport_bot/functions/logger.py
@@ -191,9 +191,13 @@ async def send_message(
 
     # Add avatar to attachments to all it to be added to the embed
     try:
-        attachments.insert(0, await author.display_avatar.to_file(filename="avatar.png"))
+        attachments.insert(
+            0, await author.display_avatar.to_file(filename="avatar.png")
+        )
     except discord.errors.HTTPException:
-        attachments.insert(0, await author.default_avatar.to_file(filename="avatar.png"))
+        attachments.insert(
+            0, await author.default_avatar.to_file(filename="avatar.png")
+        )
 
     # Make and send the embed and files
     embed = await build_embed(
