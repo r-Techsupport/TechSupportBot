6f3cc7cbecdfeac3104497c9121cb07305b1fe84
ajax146 <31014239+ajax146@users.noreply.github.com>
2026-06-05 12:34:19 -0700

    Formatting 2

--- techsupport_bot/commands/xkcd.py
+++ techsupport_bot/commands/xkcd.py
@@ -33,12 +33,12 @@ class XKCD(cogs.BaseCog):
 
     """
 
+    MOST_RECENT_API_URL: str = "https://xkcd.com/info.0.json"
+    SPECIFIC_API_URL: str = "https://xkcd.com/%s/info.0.json"
+
     xkcd: app_commands.Group = app_commands.Group(
         name="xkcd", description="Command Group for the XKCD Extension"
     )
-
-    MOST_RECENT_API_URL: str = "https://xkcd.com/info.0.json"
-    SPECIFIC_API_URL: str = "https://xkcd.com/%s/info.0.json"
 
     @xkcd.command(
         name="latest",
