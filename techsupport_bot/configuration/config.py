eaeee57655fecb18b1aa7520d92ec14e9fad6102
ajax146 <31014239+ajax146@users.noreply.github.com>
2026-06-07 10:47:53 -0700

    A bunch of changes to the config.py system to support future commands

--- techsupport_bot/configuration/config.py
+++ techsupport_bot/configuration/config.py
@@ -15,6 +15,7 @@ check_key_valid(key)
 """
 
 import json
+import os
 from pathlib import Path
 from typing import Any
 
@@ -23,6 +24,7 @@ import munch
 BASE_PATH = "configuration/"
 
 
+# Publically callable functions
 def get_config_entry(guild_id: int, key: str) -> Any:  # noqa: ANN401
     """This searches for a guild specific config entry
 
@@ -37,15 +39,15 @@ def get_config_entry(guild_id: int, key: str) -> Any:  # noqa: ANN401
         AttributeError: Raised if the passed key is not valid
     """
 
-    if not check_key_valid(key):
+    if not _check_key_valid(key):
         raise AttributeError(f"Key {key} is invalid")
 
     default_entry = get_default_config_entry(key)
 
-    if not does_guild_config_exist(guild_id):
+    if not _does_guild_config_exist(guild_id):
         return default_entry
 
-    guild_config = read_json_file(f"guild_configs/{guild_id}.json")
+    guild_config = _read_guild_json(guild_id)
 
     if key in guild_config:
         return guild_config[key]
@@ -64,16 +66,75 @@ def get_default_config_entry(key: str) -> Any:  # noqa: ANN401
     Raises:
         AttributeError: Raised if the passed key is not valid
     """
-    if not check_key_valid(key):
+    if not _check_key_valid(key):
         raise AttributeError(f"Key {key} is invalid")
 
-    default_config = read_json_file("config.default.json")
+    default_config = _read_json_file("config.default.json")
 
     return default_config[key]
 
 
-# WORKING
-def check_key_valid(key: str) -> bool:
+def get_default_config_json() -> munch.Munch:
+    """This gets a munified versions of the default config file
+
+    Returns:
+        munch.Munch: The default configuration as defined
+    """
+    return _read_json_file("config.default.json")
+
+
+def get_guild_config_json(guild_id: int) -> munch.Munch:
+    """This gets a munified version of the guild config if it exists
+
+    Args:
+        guild_id (int): The guild ID to search for
+
+    Raises:
+        AttributeError: Raised if the specified guild has no config
+
+    Returns:
+        munch.Munch: The guild config read
+    """
+    if _does_guild_config_exist(guild_id):
+        return _read_guild_json(guild_id)
+    raise AttributeError(f"No config found for guild {guild_id}")
+
+
+def write_guild_config_json(guild_id: int, new_config: munch.Munch) -> None:
+    """A function to write a guild config with a munch.Munch json
+
+    Args:
+        guild_id (int): The ID of the guild to write to
+        new_config (munch.Munch): The new config to write
+    """
+    _write_guild_json_file(guild_id, new_config)
+
+
+def edit_config_entry(guild_id: int, key: str, new_value: Any) -> None:  # noqa: ANN401
+    """This edits a config entry for a specific guild
+    If there is no guild config for a given guild, a blank config is created
+
+    Args:
+        guild_id (int): The ID of the guild to change
+        key (str): The key to write to the config
+        new_value (Any): The value of the key to write
+
+    Raises:
+        AttributeError: Raised if the passed key is not valid
+    """
+    if not _check_key_valid(key):
+        raise AttributeError(f"Key {key} is invalid")
+
+    if not _does_guild_config_exist(guild_id):
+        _write_blank_guild_config(guild_id)
+
+    guild_config = _read_guild_json(guild_id)
+    guild_config[key] = new_value
+    _write_guild_json_file(guild_id, guild_config)
+
+
+# Internal functions only
+def _check_key_valid(key: str) -> bool:
     """This will check if the key is valid and present in default config
 
     Args:
@@ -82,14 +143,14 @@ def check_key_valid(key: str) -> bool:
     Returns:
         bool: True if the key exists, false if it doesn't
     """
-    default_config = read_json_file("config.default.json")
+    default_config = _read_json_file("config.default.json")
 
     if key in default_config:
         return True
     return False
 
 
-def does_guild_config_exist(guild_id: int) -> bool:
+def _does_guild_config_exist(guild_id: int) -> bool:
     """This checks if a guild specific config file exists
 
     Args:
@@ -105,7 +166,7 @@ def does_guild_config_exist(guild_id: int) -> bool:
     return False
 
 
-def read_json_file(path: str) -> munch.Munch:
+def _read_json_file(path: str) -> munch.Munch:
     """This reads a json file from disk and parses it into a munch.Munch
     This functions assumes the json file exists
 
@@ -117,3 +178,44 @@ def read_json_file(path: str) -> munch.Munch:
     """
     with open(f"{BASE_PATH}{path}", encoding="utf-8") as file:
         return munch.munchify(json.load(file))
+
+
+def _read_guild_json(guild_id: int) -> munch.Munch:
+    """A function to get the json of a specific guild config
+
+    Args:
+        guild_id (int): The guild ID to get the config for
+
+    Returns:
+        munch.Munch: The munchified representation of the guild config
+    """
+    return _read_json_file(f"guild_configs/{guild_id}.json")
+
+
+def _write_guild_json_file(guild_id: int, json_data: munch.Munch) -> None:
+    """Writes a guild configuration file to disk.
+    This will ensure the guild_configs folder exists as well
+
+    Args:
+        guild_id (int): The ID of the guild whose configuration should be written.
+        json_data (munch.Munch): The configuration data to write.
+    """
+    path = f"{BASE_PATH}/guild_configs/{guild_id}.json"
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
+    with open(path, "w", encoding="utf-8") as file:
+        json.dump(
+            munch.unmunchify(json_data),
+            file,
+            indent=4,
+            ensure_ascii=False,
+        )
+
+
+def _write_blank_guild_config(guild_id: int) -> None:
+    """Creates a blank guild configuration file.
+
+    Args:
+        guild_id (int): The ID of the guild whose configuration should be created.
+    """
+    _write_guild_json_file(guild_id, munch.Munch())
