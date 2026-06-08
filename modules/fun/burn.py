8b42ef8f8c25a7caef7e037a596bb658dffa46f4
ajax146 <31014239+ajax146@users.noreply.github.com>
2026-06-08 03:15:07 -0700

    Remove unused import

--- modules/fun/burn.py
+++ modules/fun/burn.py
@@ -11,7 +11,6 @@ from typing import TYPE_CHECKING, Self
 
 import discord
 from discord import app_commands
-from discord.ext import commands
 
 import configuration
 from core import auxiliary, cogs
